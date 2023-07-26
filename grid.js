5456977ae9e63d8b0e58cef14332f6a7ea2fbb7b
Ihor Utkin <igorutkin2002@gmail.com>
2023-07-26 13:03:29 +0200

    Front end was rewrite from html tp js

---
+++ grid.js
@@ -0,0 +1,16 @@
+import { Cell } from "./Cell"
+
+const GRID_SIZE = 4
+const CELL_COUNT = GRID_SIZE * GRID_SIZE
+
+export class Grid{
+    constructor(gridElement){
+        this.cells = []
+
+        for(let i = 0; i < CELL_COUNT; i++){
+            this.cells.push(
+                new Cell(gridElement, i % GRID_SIZE, Math.floor(i / GRID_SIZE))
+            )
+        }
+    }
+}
